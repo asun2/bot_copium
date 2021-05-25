ec91b75a9d52f445b7921966c175ea2de668d2b4
asun2 <andrewsun2@gmail.com>
2021-05-25 19:17:01 -0400

    moved bot token to .env

--- copium.js
+++ copium.js
@@ -1,31 +1,42 @@
 const Discord = require('discord.js');
+
+// Importing this allows you to access the environment variables of the running node process
+require('dotenv').config();
+
 const client = new Discord.Client();
 const fetch = require('node-fetch');
-const starter = ';';
 
+const starter = process.env.PREFIX;
+const bot_token = process.env.CLIENT_TOKEN;
+
+console.log(bot_token)
 
 client.on('ready', () => {
+
   console.log(`Logged in as ${client.user.tag}!`);
   
 });
 
 client.on('message', msg => {
     console.log(msg.author.username);
-    // if (msg.author.username === 'asun') {
+     if (msg.author.username !== 'copium') {
         let command = msg.content.toLowerCase().split(" ");
         const author = msg.author;
-
+        msg.react('😄');
         console.log('message received');
 
         if (msg.channel.name === "any-copers" ) {
-            var url = 'https://zenquotes.io/api/random/beb2efdfc8bbecfb32f687be29893ecb95737a6f'
-            let settings = {method : "Get"};
-            let quote = ""
-            ret = fetch(url)
-                .then(res => res.json())
-                .then(json=> {msg.channel.send(json[0].q);});
-            // console.log(ret)
-            // msg.channel.send(quote)
+            console.log(command)
+            if(command[0] === "!inspire"){
+                var url = 'https://zenquotes.io/api/random/beb2efdfc8bbecfb32f687be29893ecb95737a6f'
+                let settings = {method : "Get"};
+                let quote = ""
+                ret = fetch(url)
+                    .then(res => res.json())
+                    .then(json=> {msg.channel.send(json[0].q);});
+                //console.log(ret)
+                //msg.channel.send(ret)
+            }
 
         }
 
@@ -33,11 +44,11 @@ client.on('message', msg => {
     // if(msg.author.username === "vsoltan123") {
     //     msg.react('😡');
     //     msg.reply("i lov u");
-    // }
+     }
 });
 
 
 
 //https://zenquotes.io/api/random/beb2efdfc8bbecfb32f687be29893ecb95737a6f
 
-client.login('ODM4ODMyODk1OTgzMzUzOTE3.YJA2Aw.v-14IAM4vt0rb3qjQJkCde50diA');
+client.login(bot_token);
