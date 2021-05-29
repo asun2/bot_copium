525af387320739b14e9b5e3d7a8e52a606785053
asun2 <andrewsun2@gmail.com>
2021-05-28 20:49:05 -0400

    allow for prefix changes
    updated code so far so that prefix stored in .env will be used for the rest of the code

--- copium.js
+++ copium.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
-
-// Importing this allows you to access the environment variables of the running node process
+//
 require('dotenv').config();
 
 const client = new Discord.Client();
@@ -27,7 +26,7 @@ client.on('message', msg => {
     }
     if (msg.channel.name === "any-copers" ) {
         console.log(command)
-        if(command[0] === "!inspire"){
+        if(command[0] === `${starter}inspire`){
             var url = 'https://zenquotes.io/api/random/beb2efdfc8bbecfb32f687be29893ecb95737a6f'
             let settings = {method : "Get"};
             let quote = ""
@@ -39,21 +38,26 @@ client.on('message', msg => {
         }
 
     }
-    if (command[0] === "!add-emoji") {
+    
+    if (command[0] === `${starter}add-emoji`) {
         msg.channel.send("Would you like to add an emoji?").then(message => {
             message.react('👍')
             message.react('👎');
             const filter = (reaction, user) => {
                 return ['👍', '👎'].includes(reaction.emoji.name) && user.id === msg.author.id;
             };
-            message.awaitReactions(filter, {max: 1, time: 6000, errors: ['time']}).then(collected => {
+            message.awaitReactions(filter, {max: 1, time: 60000, errors: ['time']}).then(collected => {
                 const reaction = collected.first();
                 if (reaction.emoji.name === '👍') {
-                    message.channel.send('confirmation received');
+                    message.channel.send('Upload a .png file of the emote you want to create');
+                    //turns out discord does not allow bots to upload custom emojis to the server, so this will have to change functionally
                 }
                 else {
                     message.delete();
                 }
+            })
+            .catch(collected => {
+                message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
             });
         });
     }
